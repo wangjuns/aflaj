7ad2eefdfd2ed9b9d3a6083f49fd5bf5c329c654
Wang Jun <wangjundot@gmail.com>
2022-05-29 20:17:35 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Web3 from 'web3';
+import ABI from "./abi";
+
+import { AbiItem } from 'web3-utils'
+
+declare global {
+  interface Window { ethereum: any; }
+}
+
 
 function App() {
+
+  const [num, setNum] = useState("")
+  const [num2, setNum2] = useState("")
+
+  //const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/510c76127b904e76a13ec09106657ea7"));
+  // infura not support eth_sendTransaction, change to metamask provider
+  const web3 = new Web3(window.ethereum );
+
+  //set https://github.com/KishanV/smart-contract-web3-react-demo/blob/main/src/web3-handler/index.tsx
+  
+  const address = "0xb7c941777080a611a815E77Be0A5372Be6B650D3"
+  const contract = new web3.eth.Contract(ABI as any[], address)
+
+
+  function retrieve(){
+    contract.methods.retrieve().call((_err:string, result:string) => {
+      console.log(result)
+      setNum(result)
+    })
+  }
+  
+
+  function store(){
+    contract.methods.store([12]).send({from: "0xf478c92781b7F39A23f2198F111EA5f7e1405934"}, (err:string, result:string) => {
+      console.log(err)
+      console.log(result)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my dApp!</h1>
+      <p>Your Address: {num}</p>
+
+      <button onClick={retrieve}>Retrieve</button>
+      <button onClick={store}>Store</button>
+
     </div>
   );
 }
